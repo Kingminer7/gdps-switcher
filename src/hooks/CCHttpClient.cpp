7332f707967509909555f75a2b7be581cdd11821
km7 dev <100500023+Kingminer7@users.noreply.github.com>
2024-06-18 14:25:22 -0400

    Update CCHttpClient.cpp

--- src/hooks/CCHttpClient.cpp
+++ src/hooks/CCHttpClient.cpp
@@ -7,7 +7,7 @@ class $modify(http, CCHttpClient) {
 	void send(CCHttpRequest* req)
     {
 		std::string url = req->getUrl();
-		auto newUrl = InstanceManager::get()->getServer();
+		auto newUrl = Mod::get()->getSavedValue<std::string>("server");
 		if (newUrl.empty()) {
 			newUrl = "https://www.boomlings.com/database";
 			Mod::get()->setSavedValue("server", newUrl);
