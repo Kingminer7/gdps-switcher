dc2c6735366a160a30465ead3bae1e8b5609301c
Kingminer7 <gavin@t8er.com>
2024-10-31 21:37:48 -0400

    why did no one tell me about $on_mod(DataSaved) 😭

---
+++ src/hooks/DataSave.cpp
@@ -0,0 +1,35 @@
+#include "../utils/PSUtils.hpp"
+
+using namespace geode::prelude;
+
+$on_mod(DataSaved) {
+  if (Mod::get()->getRequestedAction() == ModRequestedAction::Disable ||
+      Mod::get()->getRequestedAction() == ModRequestedAction::Uninstall ||
+      Mod::get()->getRequestedAction() ==
+          ModRequestedAction::UninstallWithSaveData ||
+      PSUtils::get()->switching == true) {
+    log::info("Deleting music library and SFX library");
+    log::info("Cache dir: {}",
+              std::string(CCFileUtils::sharedFileUtils()->getWritablePath()));
+    if (std::filesystem::exists(
+            std::string(CCFileUtils::sharedFileUtils()->getWritablePath()) +
+            "musiclibrary.dat"))
+      std::filesystem::remove(
+          std::string(CCFileUtils::sharedFileUtils()->getWritablePath()) +
+          "musiclibrary.dat");
+    else
+      log::info("Music library was not at {}!",
+                std::string(CCFileUtils::sharedFileUtils()->getWritablePath()) +
+                    "musiclibrary.dat");
+    if (std::filesystem::exists(
+            std::string(CCFileUtils::sharedFileUtils()->getWritablePath()) +
+            "sfxlibrary.dat"))
+      std::filesystem::remove(
+          std::string(CCFileUtils::sharedFileUtils()->getWritablePath()) +
+          "sfxlibrary.dat");
+    else
+      log::info("SFX library was not at {}!",
+                std::string(CCFileUtils::sharedFileUtils()->getWritablePath()) +
+                    "sfxlibrary.dat");
+  }
+}
