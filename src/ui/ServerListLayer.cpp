825c4174f41534d1a9a9fbe5dc7e9c8712a6d4e0
km7dev <gavin@t8er.com>
2025-04-30 15:22:59 -0400

    bleh

--- src/ui/ServerListLayer.cpp
+++ src/ui/ServerListLayer.cpp
@@ -75,9 +75,8 @@ bool ServerListLayer::init() {
     this->addChildAtPosition(m_bottomMenu, geode::Anchor::BottomRight, ccp(-25.f, 25.f / 4.f), false);
 
     auto scrollBg = cocos2d::extension::CCScale9Sprite::create("square02b_001.png", {0, 0, 80, 80});
-    // Terrible estimation
-    scrollBg->setColor({ 213, 151, 85 });
-    // scrollBg->setOpacity(90);
+    scrollBg->setColor({0, 0, 0});
+    scrollBg->setOpacity(90);
     scrollBg->setContentSize({380, 240});
     scrollBg->ignoreAnchorPointForPosition(false);
     scrollBg->setID("server-scroll-bg");
