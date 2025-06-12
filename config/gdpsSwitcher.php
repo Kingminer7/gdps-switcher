81b8da925d40ee1df65669227d5cf9235dfc4acb
km7 dev <100500023+Kingminer7@users.noreply.github.com>
2025-06-12 16:13:38 -0400

    Change the default icon

--- config/gdpsSwitcher.php
+++ config/gdpsSwitcher.php
@@ -8,5 +8,5 @@ $motd = "A Geometry Dash Private Server.";
 
 # The link to the icon for the server.
 # This should be a link to a png.
-$icon = "https://raw.githubusercontent.com/Kingminer7/pixel-art/refs/heads/main/pfp/pfp.png";
+$icon = "https://github.com/Kingminer7/gdps-switcher/blob/main/resources/gdlogo.png";
 ?>
